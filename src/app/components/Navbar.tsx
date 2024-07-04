8f56527a4c4cb817790e7c274359c6c0f2dd23c8
alinaincodeland <alinacuznetov@gmail.com>
2024-07-04 17:07:03 +0200

    Add content to components.

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,18 @@
+import Link from "next/link";
+import Menu from "./Menu";
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+      <div className="h-full flex items-center justify-between">
+        {/*MOBILE*/}
+        <Link href="/">
+          <div className="text-2xl tracking-wide">Kori </div>
+        </Link>
+        <Menu />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
